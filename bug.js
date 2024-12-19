b937aa21ca9a83c9f80def60612cdac6a864c38e
BugHunter-X <askzenai@gmail.com>
2024-12-19 08:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Some Tailwind classes might not work as expected if they are not properly applied to elements.
+// For example, the following code will not style the button correctly:
+<button>Click me</button>
+
+// Correct way:
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Click me</button>
+```
